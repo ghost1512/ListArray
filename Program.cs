bf3cdc3e43ce39ba80fa91640bcada8d7e11b374
BaoHG <baohuynh291@gmail.com>
2024-04-11 20:19:26 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> MyList = new List<int>();
+        for (int i = 1; i <= 10; i++)
+        {
+            MyList.Add(i);
+        }
+        Console.WriteLine(MyList.Count);
+        foreach (var item in MyList)
+        {
+            Console.Write(item + ",");
+        }
+        Console.WriteLine("");
+        //xoa phan tu so 5
+        MyList.RemoveAt(5);
+        foreach (var item in MyList)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
